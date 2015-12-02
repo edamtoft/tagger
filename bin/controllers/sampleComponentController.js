99860af8ac749b1890827ea6094e9a347b282722
Eric Damtoft <edamtoft@gmail.com>
2015-12-02 01:14:14 -0500

    Dumb mistake.

--- bin/controllers/sampleComponentController.js
+++ bin/controllers/sampleComponentController.js
@@ -1,1 +1,1 @@
-define(["system/text"],function(a){"use strict";function b(b,c){b.innerHTML=a.from("<h1>Hello {name}</h1>")}return b});
+define(["system/text"],function(a){"use strict";function b(b,c){b.innerHTML=a.from("<h1>Hello {name}</h1>",c)}return b});
