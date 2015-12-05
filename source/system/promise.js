092f3f069ab225e9020d738f62e9777336ae6cb6
Eric Damtoft <edamtoft@gmail.com>
2015-12-04 22:10:48 -0500

    Make promises always execute async

--- source/system/promise.js
+++ source/system/promise.js
@@ -4,56 +4,67 @@ define([
   
   "use strict";
   
+  var states = {
+    DEFERED: 0,
+    PENDING: 1,
+    DONE: 2
+  };
+  
   function Promise(action) {
     var _promise = this;
     
-    var pending = true;
+    var state = states.DEFERED;
     var result = null;
     var err = null;
     
     var whenDone = [];
     
     this.then = function(thenAction) {
-      if (!pending) {
-        void thenAction(err, result);
+      if (state === states.DONE) {
+        callAsync(function() { void thenAction(err, result); });
         return _promise;
       }
+      
       whenDone.push(thenAction);
+      
+      if (state === states.DEFERED) {
+        state = states.PENDING;
+        callAsync(function() { void action(fulfill, reject); });
+      }
+
       return _promise;
     };
     
-    try {
-      void action(fulfills, rejects);
-    } catch (exception) {
-      rejects(exception);
-    }
-    
-    
-    function fulfills(data){
-      if (!pending) {
+    function fulfill(data){
+      if (state === states.DONE) {
         return;
       }
-      pending = false;
+      state = states.DONE;
       result = data;
       void resolve();
     }
     
-    function rejects(exception){
-      if (!pending) {
+    function reject(exception){
+      if (state === states.DONE) {
         return;
       }
-      pending = false;
+      state = states.DONE;
       err = exception;
       void resolve();
     }
     
     function resolve() {
-      if (pending) {
-        return;
-      }
       enumerable.of(whenDone).each(function(thenAction){
         void thenAction(err, result);
       });
+    }
+    
+    function callAsync(functionToCall) {
+      if (window.setImmediate) {
+        window.setImmediate(functionToCall);
+        return;
+      }
+      window.setTimeout(functionToCall, 0);
     }
   }
   
